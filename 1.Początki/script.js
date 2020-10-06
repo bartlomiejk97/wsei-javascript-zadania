63c819f1690fb05b665d67b544aa51fcb30dbcc6
bartlomiejk97 <62827761+bartlomiejk97@users.noreply.github.com>
2020-10-06 20:21:59 +0200

    Update script.js

---
+++ 1.Początki/script.js
@@ -0,0 +1,12 @@
+// zadanie 1
+console.log("Bartłomiej");
+
+//zadanie 2
+console.log(12+32);
+console.log(52/16);
+console.log(5*2);
+
+//zadanie 3
+console.log("Hello World");
+console.log(hello world);
+console.log("Hello world);
