41c38e8109e7d560cb6b23fc88a6233e05e0cbcb
bartlomiejk97 <62827761+bartlomiejk97@users.noreply.github.com>
2020-10-07 19:20:40 +0200

    Update script.js

---
+++ 3.Operatory/script.js
@@ -0,0 +1,43 @@
+// exercise 1
+let boolOne = false;
+let boolTwo = true;
+console.log(boolOne === boolTwo);
+
+//exercise 2
+
+let numberOne = 12;
+let numberTwo = 5;
+let moduloResult = 0;
+moduloResult = numberOne % numberTwo;
+console.log(moduloResult);
+
+//exercise 3
+let string1 = "Bartłomiej";
+let string2 = "Kocik";
+let stringsResult = " ";
+stringsResult = string1 + stringsResult + string2;
+console.log(stringsResult);
+
+//exercise 4
+var someNumber = 425;
+var someString = "425";
+console.log(someNumber == someString);
+console.log(someNumber === someString);
+
+// because == compares value and === compares types variable
+
+
+/exercise 5
+let counter = 30;
+console.log(counter);
+counter ++;
+console.log(counter);
+counter --;
+console.log(counter);
+
+// exercise 6
+let number1 = 5;
+let number2 = 2;
+let result = null;
+result = number1 > number2;
+console.log(result);
