a28fc5b6754b2c4bd8736fd38ad437c433dfcb36
bartlomiejk97 <62827761+bartlomiejk97@users.noreply.github.com>
2020-10-07 19:33:16 +0200

    Update script.js

---
+++ 4.Pętle i ify/script.js
@@ -0,0 +1,40 @@
+// exercise 1
+
+let numberOne = 3;
+let numberTwo = 2;
+
+if(numberOne > numberTwo)
+  {
+    console.log(numberOne + " jest większe od " + numberTwo);
+    
+  }
+  else
+  {
+    console.log(numberOne + " jest mniejsze od " + numberTwo);
+  }
+
+// exercise 2
+
+let number1 = 1;
+let number2 = 5;
+let number3 = 3;
+
+if(number1 > number2 && number3)
+  {
+    console.log("Największe jest: "+ number1);
+  }
+  else if(number2 > number3)
+    {
+      console.log("Największe jest: "+ number2);
+    }
+  else{
+    console.log("Największe jest: "+ number3);
+  }
+  
+  // exercise 3
+  for(let i = 1; i < 11; i++)
+  {
+    console.log(i + ". I like JavaScript");
+  }
+
+
