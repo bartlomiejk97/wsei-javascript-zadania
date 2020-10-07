3c6f78c22baa176abb0261e105f380e40a6f8e6a
bartlomiejk97 <62827761+bartlomiejk97@users.noreply.github.com>
2020-10-07 19:05:29 +0200

    Update script.js

---
+++ 2. Zmienne i stałe/script.js
@@ -0,0 +1,25 @@
+// exercise 1 
+let number = 15;
+console.log(number);
+let string = "Bartłomiej";
+console.log(string);
+let numberAndString = 2 + "dwa";
+console.log(numberAndString);
+let logic = number === string;
+console.log(logic);
+let special = null;
+console.log(special);
+
+
+// exercise 2
+let number1 = 15;
+let number2 = 14;
+let sum = 0;
+sum = number1 + number2;
+console.log(sum);
+
+// exercise 3 
+let variable; 
+console.log(variable);
+
+The variable doesn't has assigned value to it
