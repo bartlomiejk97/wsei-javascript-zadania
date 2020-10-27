45bfabf654159b88197c1c904ce23cc544811258
bartlomiejk97 <62827761+bartlomiejk97@users.noreply.github.com>
2020-10-27 10:38:54 +0100

    Update script.js

--- 6.Funkcje/script.js
+++ 6.Funkcje/script.js
@@ -10,3 +10,32 @@ function my(a) {
 }
 
 console.log( my(2));
+
+/// exercise 3
+
+function getArray(arr) {
+    return arr;
+}
+
+getArray([1,2]);
+
+
+/// exercise 4
+function myString(paramSTR)
+{
+  let counter = 0;
+  let myInterval = setInterval(() => {
+      counter++;
+      if(counter == 5)
+        {
+          console.log(" Koniec! ");
+          clearInterval(myInterval);
+        }
+      else
+        {
+          console.log(paramSTR);
+        }
+  },3000); 
+}
+
+myString('Ala ma kota');
