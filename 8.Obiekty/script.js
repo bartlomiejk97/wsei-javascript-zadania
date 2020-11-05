4ed8ff8c64560ca48602db598c1362d7bc0ae63b
bartlomiejk97 <62827761+bartlomiejk97@users.noreply.github.com>
2020-11-05 17:03:32 +0100

    Update script.js

---
+++ 8.Obiekty/script.js
@@ -0,0 +1,15 @@
+// exercise 1 
+
+const car = {
+  type : "hatchback",
+  maxspeed: "230",
+  fuelusage: "7",
+  whatdoes () {
+    return " brum brum";
+  }
+}
+
+console.log(car.type);
+console.log(car.maxspeed);
+console.log(car.fuelusage);
+console.log(car.whatdoes());
