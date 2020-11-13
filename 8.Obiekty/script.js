73864d6f79cb6e230c93c536656934f4b3b59fa2
bartlomiejk97 <62827761+bartlomiejk97@users.noreply.github.com>
2020-11-13 16:49:13 +0100

    Update script.js

--- 8.Obiekty/script.js
+++ 8.Obiekty/script.js
@@ -9,3 +9,17 @@ console.log(car.name);
 console.log(car.age);
 console.log(car.maxSpeed);
 console.log(car.singpost);
+
+
+// exercise 2
+let car = {
+  name: "BMW",
+  age: 6,
+  maxSpeed: 259,
+  singpost: false,
+  changeName: function newName(newName){
+    this.name = newName;
+}
+}
+car.changeName("Fiat");
+console.log(car.name);
