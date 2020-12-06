6f493f81bebbc3e8ba4101fa7dc2d984531bb19f
bartlomiejk97 <62827761+bartlomiejk97@users.noreply.github.com>
2020-12-06 12:09:33 +0100

    Update script.js

--- 10.This/script.js
+++ 10.This/script.js
@@ -37,3 +37,42 @@ person2.addOneYear();
 person2.addOneYear();
 
 person2.showDetails();
+
+
+// exercise 2
+
+
+
+
+
+
+
+function PersonAdvanced(name, last, age, country, city, language, dishes){
+	this.name = name;
+	this.last = last;
+	this.age = age;
+	this.country = country;
+	this.city = city;
+	this.dishes = dishes;
+	
+	this.logInfo = () => {
+		for(let key in this){
+			console.log(key + " : " + this[key]);
+		}
+	}
+    
+    this.addDish = (dish) => {
+        this.dishes.push(dish);
+    }
+	
+	this.increaseAge = () => {
+		this.age++;
+	}
+}
+
+let personAdvanced = new PersonAdvanced("Andrzej", "Kowalczyk", 18, "Polska", "Gdynia", "Polski", ["Gołąbki", "Kluski", "Pomidorowa"]);
+
+personAdvanced.addDish("Zrazy");
+personAdvanced.addDish("Rosół");
+
+personAdvanced.logInfo();
