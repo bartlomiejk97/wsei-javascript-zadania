0c8243a4fd391818ebdf35743020f3d27b045e61
bartlomiejk97 <62827761+bartlomiejk97@users.noreply.github.com>
2020-12-06 12:10:27 +0100

    Update script.js

--- 10.This/script.js
+++ 10.This/script.js
@@ -109,3 +109,35 @@ const calc1=new Calculator(4,5);
 const calc2=new Calculator(4,0);
 console.log(calc1.sum());
 console.log(calc2.dzielenie());
+
+
+// exercise 4
+unction Constructor(){
+    this.number = 0;
+    
+    this.up = function() {
+        this.number++;
+    }
+    
+    this.down = function() {
+        if(this.number > 0){
+            this.number--;
+        }
+        else{
+            console.log("Jesteś na ziemi.");
+        }        
+    }
+    
+    this.showLevel = function() {
+        console.log(this.number);
+    }
+}
+
+var constructor = new Constructor();
+
+constructor.showLevel();
+constructor.up();
+constructor.up();
+constructor.showLevel();
+constructor.down();
+constructor.showLevel();
