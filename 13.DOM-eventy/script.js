0f628cb0e31ec8526204435db3b984ad4b730b31
bartlomiejk97 <62827761+bartlomiejk97@users.noreply.github.com>
2021-01-10 12:00:45 +0100

    Update script.js

--- 13.DOM-eventy/script.js
+++ 13.DOM-eventy/script.js
@@ -1,30 +1,19 @@
 let button = document.querySelector('#test-event');
 button.addEventListener("click", function (event) {
- 
   console.log(event.type);
 });
-
 button.addEventListener("mouseover", function (event) {
-  
   console.log(event.type);
 });
-
 addEventListener("mousemove", function (event){
-  
   console.log(event.type);
 });
-
 addEventListener("keypress", function (event){
-
   console.log(event.type);
 });
-
 addEventListener("scroll", function (event){
-
   console.log(event.type);
 });
-
 document.querySelector('#input-test').addEventListener("change", function (event){
-
   console.log(event.type);
 });
