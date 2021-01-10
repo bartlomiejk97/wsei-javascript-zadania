82ba0b57d9b332ced86e1c9738cc195ec57cd823
bartlomiejk97 <62827761+bartlomiejk97@users.noreply.github.com>
2021-01-10 12:03:39 +0100

    Update script.js

--- 13.DOM-eventy/script.js
+++ 13.DOM-eventy/script.js
@@ -1,3 +1,4 @@
+//exercise 1
 let button = document.querySelector('#test-event');
 button.addEventListener("click", function (event) {
   console.log(event.type);
@@ -17,3 +18,54 @@ addEventListener("scroll", function (event){
 document.querySelector('#input-test').addEventListener("change", function (event){
   console.log(event.type);
 });
+
+/exercise 2 
+function pressbutton() {
+    const buttonEx2 = document.getElementById('ex2')
+    buttonEx2.addEventListener('click', enterText => {
+        let dataText = enterText.target.dataset.text;
+        let belowElement = enterText.target.nextElementSibling;
+        
+        belowElement.innerText = dataText
+    })
+}
+pressbutton();
+
+// exercise 3
+  function switchColor(){
+    const myElement = document.getElementById('ex3');
+    myElement.addEventListener('mouseover', (event) => {
+        event.target.style.backgroundColor = 'blue';
+    });
+    myElement.addEventListener('mouseout', (event) => {
+        event.target.style.backgroundColor = 'red';
+    });
+}
+switchColor();
+
+//exercise 4
+const inputError = document.querySelector('#ex3-err');
+const input = document.querySelector('#input-test');
+input.addEventListener('keyup', (e) => {
+	const reg = /[0-9]/gm;
+	const checkIfNumber = e.target.value.match(reg);
+	if (checkIfNumber !== null) {
+		inputError.innerText = 'You cannot enter numbers here';
+	} else {
+		inputError.innerText = '';
+	}
+});
+
+// exercise 5 
+const divEx5 = document.getElementById('ex5');
+const buttonEx5 = document.getElementById('ex5-button');
+let numberOfClick = 0
+buttonEx5.addEventListener('click', function counting() {
+    numberOfClick++
+    if(numberOfClick ==  10) {
+        buttonEx5.removeEventListener('click', counting)
+    }
+    else {
+        divEx5.innerHTML = numberOfClick
+    }
+})
